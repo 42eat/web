45d8ef1f0081ca1509f9dcb9f2f385f6d588f1e5
V-Fries <vincentfries@proton.me>
2025-12-16 07:42:08 +0100

    feat: Setup base files and dependencies for axum backend

---
+++ back/src/main.rs
@@ -0,0 +1,13 @@
+mod routes;
+
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.expect("Server crashed");
+}
