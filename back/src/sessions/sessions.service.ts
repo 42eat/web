296523f98e195c03d37f446cc0a5ccacb131ad36
Liammmmmmmm <lilefebv@student.42lyon.fr>
2026-03-13 16:26:23 +0100

    add cron to remove expired sessions

--- back/src/sessions/sessions.service.ts
+++ back/src/sessions/sessions.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
 import { PrismaService } from 'src/core/prisma/prisma.service';
 
 @Injectable()
 export class SessionsService {
 	constructor(private readonly prisma: PrismaService) {}
+	
+	@Cron('0 * * * *')
+	async cleanupExpiredSessions() {
+		const result = await this.prisma.session.deleteMany({
+			where: { expires_at: { lt: new Date() } }
+		});
+		console.log(`Deleted ${result.count} expired sessions`);
+	}
 
 	public async create(memberId: number, refreshToken: string, expiresAt: Date, userAgent: string, ipAddress: string) {
 		return this.prisma.session.create({ data: { member_id: memberId, refresh_token: refreshToken, expires_at: expiresAt, user_agent: userAgent, ip_address: ipAddress } });
@@ -17,4 +26,10 @@ export class SessionsService {
 		return this.prisma.session.delete({ where: { id: sessionId } });
 	}
 
+	async deleteUserExpiredSessions(memberId: number) {
+		return await this.prisma.session.deleteMany({
+			where: { expires_at: { lt: new Date() } }
+		});
+	}
+	
 }
