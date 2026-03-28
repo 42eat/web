4657c9d0cf371dc0a3a3cc94374284178533cdbc
saumon-brule <96207807+saumon-brule@users.noreply.github.com>
2026-03-28 22:58:50 +0100

    feat: logo cool

--- front/src/pages/auth/AuthLayout.tsx
+++ front/src/pages/auth/AuthLayout.tsx
@@ -2,12 +2,12 @@ import { A, RouteSectionProps, useLocation } from "@solidjs/router"
 import { Transition } from "solid-transition-group"
 
 import "./AuthLayout.scss"
+import NeonLogo from "~/components/common/NeonLogo"
 
 export default function AuthLayout(props: RouteSectionProps) {
-	const location = useLocation()
-
 	return <div id="auth-page">
 		<div class="auth-hero">
+			<NeonLogo />
 		</div>
 		<section class="auth-section">
 			<header class="top-details"></header>
