662a40f18d48d4267bed4fbf344b0436878d1465
saumon-brule <96207807+saumon-brule@users.noreply.github.com>
2026-03-29 03:18:04 +0200

    feat: login page and error handling

--- front/src/components/ui/TextInput/TextInput.tsx
+++ front/src/components/ui/TextInput/TextInput.tsx
@@ -1,16 +1,19 @@
-import { createSignal, JSX, Show, splitProps, createMemo } from "solid-js";
+import { createSignal, JSX, Show, splitProps, createMemo, createUniqueId } from "solid-js";
 
 import "./TextInput.scss"
 
 type TextInputProps = Omit<JSX.IntrinsicElements["input"], "onInvalid" | "onInput"> & {
 	invalidMessage?: JSX.Element,
+	invalid?: boolean,
 	onInvalid?: ((event: Event) => void),
 	onInput?: ((event: Event) => void),
 }
 
 export default function TextInput(props: TextInputProps) {
 	const [isInvalid, setIsInvalid] = createSignal(false);
-	const [local, rest] = splitProps(props, ["class", "invalidMessage", "onInvalid", "onInput"]);
+	const [local, rest] = splitProps(props, ["class", "invalid", "invalidMessage", "onInvalid", "onInput"]);
+
+	const ariaMessageId = createUniqueId();
 
 	function handleInvalid(event: Event) {
 		if (local.onInvalid) local.onInvalid(event);
@@ -24,12 +27,12 @@ export default function TextInput(props: TextInputProps) {
 		setIsInvalid(false);
 	}
 
-	const invalidMessageId = createMemo(() => crypto.randomUUID());
+	const invalid = () => props.invalid ?? isInvalid();
 
 	return <div class={`text-input ${local.class ?? ""}`}>
-		<input onInvalid={handleInvalid} aria-invalid={isInvalid()} onInput={handleInput} aria-describedby={invalidMessageId()} {...rest} />
-		<Show when={isInvalid()}>
-			<p class="invalid-message" id={`${invalidMessageId()}`}>
+		<input onInvalid={handleInvalid} onInput={handleInput} aria-invalid={invalid()} aria-describedby={invalid() ? ariaMessageId : undefined} {...rest} />
+		<Show when={invalid()}>
+			<p class="invalid-message" id={ariaMessageId}>
 				{local.invalidMessage ?? "Invalid input"}
 			</p>
 		</Show>
--- front/src/pages/auth/LoginForm.tsx
+++ front/src/pages/auth/LoginForm.tsx
@@ -2,18 +2,18 @@ import { A } from "@solidjs/router";
 import Button from "../../components/ui/Button";
 import TextInput from "~/components/ui/TextInput";
 import { client } from "~/api/client";
-import { createSignal } from "solid-js";
+import { createSignal, JSXElement, Show } from "solid-js";
 import { authActions } from "~/store/auth.store";
 
 import "./LoginForm.scss"
 
 export default function LoginForm() {
-	const [username, setUsername] = createSignal("");
-	const [password, setPassword] = createSignal("");
+	const [username, setUsername] = createSignal("a@a.fr");
+	const [password, setPassword] = createSignal("dzq");
+	const [error, setError] = createSignal<JSXElement>(null);
 
-	console.log("test")
 	const loginMutation = client.auth.login.createMutation();
-	console.log("test2");
+
 
 	const handleSubmit = async (e: SubmitEvent) => {
 		e.preventDefault();
@@ -22,17 +22,37 @@ export default function LoginForm() {
 				authActions.login(data.body.accessToken);
 			},
 			onError: (e) => {
-				console.error(e);
+				if (e.status === 401) {
+					switch (e.body.code) {
+						case "INTRA_ONLY_ACCOUNT":
+							setError(<>This account is only accessible with the <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">42's Intra login</a></>);
+							break;
+						case "INVALID_CREDENTIALS":
+							setError("Invalid credential combination");
+							break;
+						default:
+							setError("An error occured. There is nothing to do :/");
+					}
+				} else {
+					setError("An error occured. There is nothing to do :/");
+				}
 			}
 		})
-	}
+	};
 
 	return <form onSubmit={handleSubmit} id="login-form">
 		<div class="login-inputs">
-			<TextInput type="text" placeholder="Email or 42's login" value={username()} onInput={(e) => setUsername((e.target as HTMLInputElement).value)} />
-			<TextInput type="password" placeholder="Password" value={password()} onInput={(e) => setPassword((e.target as HTMLInputElement).value)} />
+			<TextInput type="email" placeholder="Email" required invalidMessage="Valid email required" value={username()} onInput={(e) => setUsername((e.target as HTMLInputElement).value)} />
+			<TextInput type="password" placeholder="Password" required invalidMessage="Password required" value={password()} onInput={(e) => setPassword((e.target as HTMLInputElement).value)} />
+			<Show when={error()}>
+				<p class="invalid-message" role="alert">
+					{error()}
+				</p>
+			</Show>
 		</div>
-		<p class="login-details">Need an account? <A href="/auth/register">Register</A>.</p>
+		<div class="login-details">
+			<p class="auth-switcher">Need an account? <A href="/auth/register">Register</A>.</p>
+		</div>
 		<Button type="submit" >Login</Button>
 		<div class="separator">
 			<hr />
