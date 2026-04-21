3139efe5a4aa36a84a6635b3592e33f40fa466a0
saumon-brule <96207807+saumon-brule@users.noreply.github.com>
2026-04-21 07:49:27 +0200

    feat: authStore alway initialized with token if possible (either by getting it from sessionStorage, or by reregenarating it from refresh token) + loading splash screen for coolness

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,19 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
-import type { Component } from "solid-js";
+import { Show, type Component } from "solid-js";
 import { AppRouter } from "./routes";
 import { I18nProvider } from "./i18n/context";
+import { initialized } from "./store/auth.store";
+import LoadingSplash from "./pages/Loading/LoadingSplash";
 
 const client = new QueryClient();
 
 const App: Component = () => {
 	return <I18nProvider>
-		<QueryClientProvider client={client}>
-			<AppRouter />
-		</QueryClientProvider>
+		<Show when={initialized()} fallback={<LoadingSplash/>}>
+			<QueryClientProvider client={client}>
+				<AppRouter />
+			</QueryClientProvider>
+		</Show>
 	</I18nProvider>;
 };
 
--- front/src/pages/Home/Home.tsx
+++ front/src/pages/Home/Home.tsx
@@ -1,6 +1,13 @@
+import { Show } from "solid-js";
+import { client } from "~/api/client";
 
 export default function Home() {
+	const profile = client.members.profile.createQuery(() => ["members", "profile"], () => ({}));
+
 	return <div style={{ background: "red" }}>
 		HOME, SWEET HOME ...
+		<Show when={profile.isFetched}>
+			<pre><code>{JSON.stringify(profile.data?.body, null, 2)}</code></pre>
+		</Show>
 	</div>;
 }
