20bd76b6ab234645dca7a93266fab7c54b2b9948
saumon-brule <96207807+saumon-brule@users.noreply.github.com>
2026-04-23 05:29:06 +0200

    feat: add logout button on home

--- front/src/pages/Home/Home.tsx
+++ front/src/pages/Home/Home.tsx
@@ -1,13 +1,23 @@
 import { Show } from "solid-js";
 import { client } from "~/api/client";
+import Button from "~/components/ui/Button";
+import { authActions } from "~/store/auth.store";
 
 export default function Home() {
 	const profile = client.members.profile.createQuery(() => ["members", "profile"], () => ({}));
+
+	const logoutMutation = client.auth.logout.createMutation();
+
+	function logout() {
+		logoutMutation.mutate({});
+		authActions.logout();
+	}
 
 	return <div style={{ background: "red" }}>
 		HOME, SWEET HOME ...
 		<Show when={profile.isFetched}>
 			<pre><code>{JSON.stringify(profile.data?.body, null, 2)}</code></pre>
+			<Button onClick={logout}>logout</Button>
 		</Show>
 	</div>;
 }
