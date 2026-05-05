9ef0edada7c202da407683ec97599c9b581a1312
ebini <96207807+saumon-brule@users.noreply.github.com>
2026-05-05 14:17:17 +0200

    feat: technically valid toaster

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,16 +4,19 @@ import { AppRouter } from "./routes";
 import { I18nProvider } from "./i18n/context";
 import { initialized } from "./store/auth.store";
 import LoadingSplash from "./pages/Loading/LoadingSplash";
+import { ToasterProvider } from "./components/ui/Toaster/Toaster";
 
 const client = new QueryClient();
 
 const App: Component = () => {
 	return <I18nProvider>
-		<Show when={initialized()} fallback={<LoadingSplash/>}>
-			<QueryClientProvider client={client}>
-				<AppRouter />
-			</QueryClientProvider>
-		</Show>
+		<ToasterProvider>
+			<Show when={initialized()} fallback={<LoadingSplash/>}>
+				<QueryClientProvider client={client}>
+					<AppRouter />
+				</QueryClientProvider>
+			</Show>
+		</ToasterProvider>
 	</I18nProvider>;
 };
 
--- front/src/components/ui/Toaster/Toaster.tsx
+++ front/src/components/ui/Toaster/Toaster.tsx
@@ -1,25 +1,32 @@
 import { createSignal, For, JSXElement } from "solid-js";
 
-const DEFAULT_TTL = 5000;
+const DEFAULT_TTL = 1000;
 
 interface SummonToastOptions {
 	customClass?: string;
-	/** The time the Toast stay in ms */
+	/** The time the Toast stay in ms. */
 	ttl?: number;
 	ref?: (element: HTMLDialogElement) => void;
 }
 
 interface Toast {
+	id: number;
 	element: JSXElement;
 	deathTime: number;
 }
 
 const [toasts, setToasts] = createSignal<Toast[]>([]);
 
+let toastId = 0;
+
 export function summonToast(content: JSXElement, options?: SummonToastOptions) {
 	const ttl = options?.ttl ?? DEFAULT_TTL;
-	setToasts((prev) => [...prev, { element: content, deathTime: Date.now() + ttl }]);
+	const thisToastId = toastId++;
+	setToasts((prev) => [...prev, { id: thisToastId, element: content, deathTime: Date.now() + ttl }]);
+	setTimeout(() => setToasts((prev) => prev.filter((toast) => toast.id !== thisToastId)), ttl);
 }
+
+(window as unknown as { summonToast: typeof summonToast }).summonToast = (() => summonToast(<div style={{ padding: "5px", background: "red" }}>TEST</div>));
 
 export function ToasterProvider(props: { children: JSXElement }) {
 	return <>
